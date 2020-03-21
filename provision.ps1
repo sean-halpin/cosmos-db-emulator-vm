9f48816565bc8b446acdb1bb0cd327ab43a28c5a
Sean <pulpfictionost@gmail.com>
2020-03-21 03:10:05 +0000

    Cosmos DB Emulator - Delayed Start

--- provision.ps1
+++ provision.ps1
@@ -10,5 +10,6 @@ Write-Output "Installing Cosmos DB Emulator as a Service"
 C:\vagrant\nssm\nssm-2.24\win64\nssm.exe install cosmosdbemulator "C:\vagrant\startup.cmd"
 C:\vagrant\nssm\nssm-2.24\win64\nssm.exe set cosmosdbemulator Start SERVICE_DELAYED_AUTO_START
 C:\vagrant\nssm\nssm-2.24\win64\nssm.exe set cosmosdbemulator Type SERVICE_INTERACTIVE_PROCESS
+C:\vagrant\nssm\nssm-2.24\win64\nssm.exe start cosmosdbemulator
 
 Write-Output "Done"
