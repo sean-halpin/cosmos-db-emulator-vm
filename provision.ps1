e37ce634d6345c18cd3d07d5e06568de3fc9e016
Sean <pulpfictionost@gmail.com>
2020-03-21 03:07:40 +0000

    Cosmos DB Emulator - Delayed Start

--- provision.ps1
+++ provision.ps1
@@ -1,16 +1,14 @@
-#https://aka.ms/cosmosdb-emulator
-(New-Object System.Net.WebClient).DownloadFile('https://aka.ms/cosmosdb-emulator', 'C:\vagrant\cosmos.msi')
-
 Write-Output "Intalling Cosmos DB Emulator"
+(New-Object System.Net.WebClient).DownloadFile('https://aka.ms/cosmosdb-emulator', 'C:\vagrant\cosmos.msi')
 Start-Process -wait C:\vagrant\cosmos.msi -ArgumentList "/quiet"
 
 Write-Output "Installing NSSM"
 (New-Object System.Net.WebClient).DownloadFile('http://nssm.cc/release/nssm-2.24.zip', 'C:\vagrant\nssm.zip')
-Expand-Archive C:\vagrant\nssm.zip -DestinationPath C:\vagrant\nssm
+Expand-Archive C:\vagrant\nssm.zip -DestinationPath C:\vagrant\nssm -Force
 
 Write-Output "Installing Cosmos DB Emulator as a Service"
 C:\vagrant\nssm\nssm-2.24\win64\nssm.exe install cosmosdbemulator "C:\vagrant\startup.cmd"
-C:\vagrant\nssm\nssm-2.24\win64\nssm.exe set cosmosdbemulator Start SERVICE_DELAYED_START
+C:\vagrant\nssm\nssm-2.24\win64\nssm.exe set cosmosdbemulator Start SERVICE_DELAYED_AUTO_START
 C:\vagrant\nssm\nssm-2.24\win64\nssm.exe set cosmosdbemulator Type SERVICE_INTERACTIVE_PROCESS
 
 Write-Output "Done"
