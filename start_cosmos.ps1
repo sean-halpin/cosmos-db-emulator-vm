f922742840de9ea6c81e45366f191548e20b817e
Sean <pulpfictionost@gmail.com>
2020-03-26 19:55:20 +0000

    Make sure Cosmos Emulator Service Starts

--- start_cosmos.ps1
+++ start_cosmos.ps1
@@ -2,5 +2,10 @@ Write-Output "Starting Cosmos DB Emulator Service"
 
 Import-Module -Name "C:\Program Files\Azure Cosmos DB Emulator\PSModules\Microsoft.Azure.CosmosDB.Emulator" -Verbose
 
-Start-CosmosDbEmulator -AllowNetworkAccess -NoFirewall -NoUI -Key C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==
+$cosmosStatus = "Stopped"
+Do {
+    $cosmosStatus = Get-CosmosDbEmulatorStatus
+    Start-CosmosDbEmulator -AllowNetworkAccess -NoFirewall -NoUI -Key C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw== 
+} while($cosmosStatus -eq "Stopped")
+
 
