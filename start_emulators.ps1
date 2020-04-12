38fb3682d1beb9386106aea2432b63d862e58ccb
Sean Halpin <sean.halpin@jet.computer>
2020-04-12 16:00:48 +0100

    Start Storage Emulator

--- start_emulators.ps1
+++ start_emulators.ps1
@@ -6,4 +6,8 @@ $cosmosStatus = "Stopped"
 Do {
     $cosmosStatus = Get-CosmosDbEmulatorStatus
     Start-CosmosDbEmulator -AllowNetworkAccess -NoFirewall -NoUI -Key C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw== 
+
+    cmd.exe /c "C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe init /forceCreate"
+    cmd.exe /c "C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe start"
+
 } while($cosmosStatus -eq "Stopped")
