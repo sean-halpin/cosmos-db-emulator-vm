6a8aac89b8f5d95148eae6dc7089886ed4c6ea5f
Sean Halpin <do-not-reply@example.com>
2020-04-12 17:13:53 +0100

    Sample App - Test Emulator

--- start_emulators.ps1
+++ start_emulators.ps1
@@ -4,7 +4,7 @@ Import-Module -Name "C:\Program Files\Azure Cosmos DB Emulator\PSModules\Microso
 
 $cosmosStatus = "Stopped"
 Do {
-    $cosmosStatus = Get-CosmosDbEmulatorStatus
+    # $cosmosStatus = Get-CosmosDbEmulatorStatus
     Start-CosmosDbEmulator -AllowNetworkAccess -NoFirewall -NoUI -Key C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw== 
 
     cmd.exe /c "C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe init /forceCreate"
